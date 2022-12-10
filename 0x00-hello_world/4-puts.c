597d785af2352ecb799e1786c7caa67c6a336a4f
Zuzile87 <zuzilen43@gmail.com>
2022-12-09 20:05:56 -0800

    first project C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main -Print a line of code using puts
  *
- * Return:|0|
+ * Retur: 0
 */
- 
+
 int main(void)
 {
- 	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
